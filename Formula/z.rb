1bf091afa2ab4d85eabfc259a7c14a3b9bb663ea
Zhenya Khvan <zhenya.khvan@gmail.com>
2025-04-19 15:23:39 -0400

    chore: update z to 0.1.1

--- Formula/z.rb
+++ Formula/z.rb
@@ -1,8 +1,8 @@
 class Z < Formula
   desc      "Zhenya's CLI"
   homepage  "https://github.com/zkhvan/z"
-  url       "https://github.com/zkhvan/z/archive/refs/tags/v0.1.0.tar.gz"
-  sha256    "891cd276903debf879857d631cf73f3b7e796a5f421143d0b8f1b3ec782d6151"
+  url       "https://github.com/zkhvan/z/archive/refs/tags/v0.1.1.tar.gz"
+  sha256    "d7733c30fb3a31e60f39be5784adfaddc3531c22ede0a2ce8caa8e990887a2c9"
   license   "MIT"
 
   head      "https://github.com/zkhvan/z.git", branch: "main"
