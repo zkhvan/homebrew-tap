80e919d6f54b5f6cee2af592b15dd1a8c8a881f6
Zhenya Khvan <zhenya.khvan@gmail.com>
2025-04-19 19:11:50 -0400

    chore: mark z as a conflict

--- Formula/z.rb
+++ Formula/z.rb
@@ -4,10 +4,10 @@ class Z < Formula
   url       "https://github.com/zkhvan/z/archive/refs/tags/v0.1.1.tar.gz"
   sha256    "d7733c30fb3a31e60f39be5784adfaddc3531c22ede0a2ce8caa8e990887a2c9"
   license   "MIT"
-
   head      "https://github.com/zkhvan/z.git", branch: "main"
 
   depends_on "go" => :build
+  conflicts_with "z", because: "both install z"
 
   deny_network_access! [:postinstall, :test]
 
